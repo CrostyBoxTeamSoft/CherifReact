51f65251c01c4646644fc35dc186adcfd787c56b
MohametCherif <faycherif8@gmail.com>
2020-12-08 15:20:42 +0100

    faites que ca marche

---
+++ projectTest/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React  from 'react';
+//import Search from './Components/Search';
+//import Home from './Components/Home';
+//import AddPet from './Components/AddPet';
+//import Felix from './Components/Felix';
+//import FelixProg from './Components/FelixProg';
+import Compte from './Components/Compte';
+//import Resume from './Components/Resume';
+
+import { StyleSheet, Text, View,Image } from 'react-native';
+//import {ProgressBar} from'react-native-progress-bar';
+
+export default function App() {
+  return (
+    //  <Search/>
+     //<Home/>
+     //<AddPet/>
+     //<Felix/>
+     //<FelixProg/>
+     <Compte/>
+     //<Resume/>
+  );
+}
+
+/*class App extends Component {
+  render() {
+    return(
+      <div className="App">
+      <Search/>
+      //<TestForm/>
+      </div>
+    )
+  }
+}*/
